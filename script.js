f2e778ac0f97f674824d509be14e32c851021aeb
SimpleBeat <simplebeat@simplebeat.com>
2020-07-20 16:57:04 +0300

    adjustments to mobile layout and js refactor

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const header = document.getElementById("myHeader");
 const sticky = header.offsetTop;
 
-// On page load 
-window.onscroll = function() { checkOffset() };
 
+
+
+// Helper functions
 function checkOffset() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
 }
 
+
+// On page load 
+window.onscroll = function() { checkOffset() };
