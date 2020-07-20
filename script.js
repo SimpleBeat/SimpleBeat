c966e6b87859e058d0f877a11195f3873442cb39
SimpleBeat <simplebeat@simplebeat.com>
2020-07-20 16:38:17 +0300

    add navigation tab

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-// On page load
+const header = document.getElementById("myHeader");
+const sticky = header.offsetTop;
 
+// On page load 
+window.onscroll = function() { checkOffset() };
+
+function checkOffset() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 
