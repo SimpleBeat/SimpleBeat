daf6984987e303a6e4427528660963e0a0daccaa
SimpleBeat <simplebeat@simplebeat.com>
2020-07-21 12:53:56 +0300

    add page generation from an array of elements

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-const header = document.getElementById("myHeader");
-const sticky = header.offsetTop;
+const header = document.getElementById("myHeader")
+const sticky = header.offsetTop
 
-const paper = document.getElementById("myPaper");
+const paper = document.getElementById("myPaper")
+let currentPage = "W"
+
+const articlesList = []; // list of all articles from articles.txt
+const projectsList = []; // list of all projects from projects.txt
+
+const welcomeLink = document.getElementById("welcome")
+const projectsLink = document.getElementById("projects")
+const articlesLink = document.getElementById("articles")
+
+const welcomePage = [
+    "Welcome to SimpleBeat!",
+    "<div class='article-image to-left'><img src='img/ilya.jpg' alt='ilya avatar'></div>",
+    "I am a web developer with experience in design and mobile software engineering.",
+    "I've been building, deploying, and maintaining websites for over <b>20 years</b>."
+]
+
+welcomeLink.addEventListener("click", showPage("W"))
+projectsLink.addEventListener("click", showPage("P"))
+articlesLink.addEventListener("click", showPage("A"))
 
 // Helper functions
+// ----------------
+
+// makes the menu stick to the top of the page when scrolling
 function checkOffset() {
     if (window.pageYOffset > sticky) {
         header.classList.add("sticky");
@@ -12,6 +34,66 @@ function checkOffset() {
     }
 }
 
+// Main functions
+// --------------
+
+// loads a list of all articles from a text file
+function loadArticlesList() {
+
+}
+
+// loads a list of all projects from a text file
+function loadProjectsList() {
+
+}
+
+// constructs and renders page using an array of elements
+function constructPage(elements) {
+    const article = document.createElement("div")
+    const title = document.createElement("div")
+    const text = document.createElement("div")
+    const divider = document.createElement("div")
+
+    article.classList.add("article")
+    title.classList.add("article-title")
+    text.classList.add("article-body")
+    divider.classList.add("divider")
+
+    title.innerText = welcomePage[0]
+    let allText = ""
+    for (let i=1; i<welcomePage.length; i++) {
+        allText += "<p>"+welcomePage[i]+"</p>"
+    }
+    text.innerHTML = allText
+    
+
+    article.append(title)
+    article.append(text)
+    article.append(divider)
+    paper.append(article)
+}
+
+// displays Welcome, Articles, or Projects pages ("W", "A", "P")
+function showPage(x) {
+    if (currentPage === x) {
+        return
+    }
+    switch (x) {
+        case "W":
+            currentPage = x
+            constructPage(welcomePage)
+            break;
+        case "A":
+            currentPage = x
+            break;
+
+        case "P":
+            currentPage = x
+            break;
+        default:
+            break;
+    }
+}
 
 function createHelloDiv() {
     const article = document.createElement("div")
@@ -40,5 +122,8 @@ function createHelloDiv() {
 
 // On page load 
 window.onscroll = function() { checkOffset() };
-createHelloDiv()
-createHelloDiv()
+
+loadArticlesList()
+loadProjectsList()
+
+constructPage(welcomePage)
