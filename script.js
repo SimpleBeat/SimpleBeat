2495f317e92aea9d4b9f618dd88cbd69cd328370
SimpleBeat <simplebeat@simplebeat.com>
2020-07-24 20:33:22 +0300

    add js to show screenshots in projects

---
+++ script.js
@@ -0,0 +1,26 @@
+let counters = {
+    'face': 0
+}
+const faceFinderElement = document.getElementById("faceFinder")
+faceFinderElement.addEventListener('click', function() { showScreenShot('face') })
+
+function showScreenShot(elementName) {
+    const text = document.getElementById(elementName+'Text')
+    const img = document.getElementById(elementName+'Image')
+    
+    counters[elementName]++
+    
+    if (counters[elementName] > 3) { counters[elementName] = 0 }
+
+    const count = counters[elementName]
+    if (count == 0) {
+        img.style.display = 'none'
+        text.style.display = 'block'
+    } else {
+        text.style.display = 'none'
+        img.style.display = 'block'
+        const imageName = 'img/'+elementName+count+'.png'
+        img.src = imageName
+        console.log(img)
+    }
+}
